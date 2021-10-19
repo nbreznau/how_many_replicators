cf2afcafa3574fb55fc86d482b4fd62785e54a43
Nate Breznau <breznau.nate@gmail.com>
2021-10-19 10:08:28 +0200

    Still working on auto render

--- all.R
+++ all.R
@@ -1,7 +1,7 @@
 library("rmarkdown")
 library("here")
 
-# Workflow step: 2. Workflow
+# Render HTML
 rmarkdown::render(here::here("01_Data_Prep.Rmd"))
 rmarkdown::render(here::here("02_Analysis.Rmd"))
 
