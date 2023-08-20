e5c077832306e1d852b09b31ba208dc8e6f413b5
Hung Nguyen <vhnh1607@gmail.com>
2023-08-20 21:37:59 +0200

    Update Readme

--- all.R
+++ all.R
@@ -1,12 +1,12 @@
-install.packages(c("here","renv"))
+install.packages(c("here","renv", "rmarkdown"))
 library("rmarkdown")
 library("here")
 library("renv")
+# Render HTML
+#rmarkdown::render(here::here("code","01_Data_Prep.Rmd"))
+#rmarkdown::render(here::here("code","02_Analysis.Rmd"))
 
 ## Load R-Environment state from .lock file
-
+renv::activate()
 renv::restore()
 
-# Render HTML
-rmarkdown::render(here::here("code","01_Data_Prep.Rmd"))
-rmarkdown::render(here::here("code","02_Analysis.Rmd"))
